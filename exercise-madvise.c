39b121b1dacfe8d1c1570022c2712dbb993de7e5
John Henry Deppe <deppe@ece.ubc.ca>
2023-05-10 21:47:11 -0700

    madvise smokewagon over both touched and untouched pages

--- exercise-madvise.c
+++ exercise-madvise.c
@@ -5,31 +5,36 @@
 #include <sys/mman.h>
 
 #define PAGE_SIZE 4096
-#define PAGES     8
+#define PAGES     8         // even number
 
 int main(void) {
     char* ptr;
-    int err;
+    int result;
 
     // allocate: https://man7.org/linux/man-pages/man3/malloc.3.html
-    err = posix_memalign( (void**)&ptr, PAGE_SIZE, PAGES*PAGE_SIZE );
-    if (err != 0) {
-        printf("posix_memaligned() failed and returned: %d", err);
+    result = posix_memalign( (void**)&ptr, PAGE_SIZE, PAGES*PAGE_SIZE );
+    if (result != 0) {
+        printf("posix_memaligned() failed and returned: %d", result);
         return -1;
     }
-    
+
+    // touch first half:
+    for (long i = 0; i < PAGES*PAGE_SIZE/2; i++ ) {
+        ptr[i] = 'x';
+    }
+    printf("touched first half of allocated memory.\n");
 
     // madvise: https://man7.org/linux/man-pages/man2/madvise.2.html
-    int result = madvise(ptr, PAGES*PAGE_SIZE, 26); // 26 advises MADV_PRIVATE_TLB
+    result = madvise(ptr, PAGES*PAGE_SIZE, 26); // 26 advises MADV_PRIVATE_TLB
     if (result != 0) {
         printf("madvise returned: %d, errno: %d, strerror: %s\n", result, errno, strerror(errno));
     }
-    
 
-    // touch:
-    for (long i = 0; i < PAGES*PAGE_SIZE; i++ ) {
+    // touch second half:
+    for (long i = PAGES*PAGE_SIZE/2; i < PAGES*PAGE_SIZE; i++ ) {
         ptr[i] = 'x';
     }
+    printf("touched all allocated memory.\n");
 
     // free
     free(ptr);
@@ -38,9 +43,5 @@ int main(void) {
 }
 
 /* Things to try:
- * 1. Allocate, madvise, touch
- * 2. Allocate, touch, madvise
- * 3. Allocate multiple pages, touch some, madvise others
- * 4. Various permutations of madvise and touching between allocating and freeing.
- * 5. Clearing the madvise (MADV_NORMAL?)
+ * 1. Clearing the madvise (MADV_NORMAL?)
  */
