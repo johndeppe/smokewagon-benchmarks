fdc5d53c08288eef6c4539df5acfd31e90768645
John Henry Deppe <deppe@ece.ubc.ca>
2025-12-10 15:46:11 -0800

    * Add readwrite variant, which just calls mprotect() with PROT_READ | PROT_WRITE.   This shows maximum mprotect() throughput with no shootdowns slowing us down.
    * Pin threads to CPUs (again)
      Hopefully helps reduce noise in our results.
    
    * Allocate hugepage per thread instead of just one large area for all threads.
      Avoids choking on >64MB allocations because Smokewagon doesn't support hugepages yet.

--- microbenchmark-mprotect.c
+++ microbenchmark-mprotect.c
@@ -31,6 +31,9 @@ long threads = 4;
 long duration = 5;
 long end;
 
+int protread;
+int protwrite;
+
 void* test_smokewagon(void* info_ptr) {
     struct per_thread_info* my_info = info_ptr;
     long tid = my_info->tid;
@@ -43,11 +46,11 @@ void* test_smokewagon(void* info_ptr) {
         // mprotect: https://man7.org/linux/man-pages/man2/mprotect.2.html
 
         // write page
-        my_info->return_value = mprotect(my_info->my_page, PAGE_SIZE, PROT_WRITE);
+        my_info->return_value = mprotect(my_info->my_page, PAGE_SIZE, protwrite);
         my_info->my_page[0] = local_counter;
 
         // read page
-        my_info->return_value = mprotect(my_info->my_page, PAGE_SIZE, PROT_READ);
+        my_info->return_value = mprotect(my_info->my_page, PAGE_SIZE, protread);
         assert(my_info->my_page[0] == local_counter); // read page
 
         local_counter++;
@@ -60,7 +63,8 @@ void* test_smokewagon(void* info_ptr) {
 
 int main(int argc, char *argv[]) {
     struct per_thread_info thread_infos[MAX_THREADS];
-    long results[MAX_THREADS][2] = {0}; // second dimension is smokewagon.  0 is off and 1 is on.
+    long results[MAX_THREADS][2][2] = {0}; // second dimension is smokewagon.  0 is off and 1 is on.
+                                           // third dimension is readwrite, 0 is shootdown and 1 is no_prot_change so no shootdown
 
     // check opts
     int opt;
@@ -108,38 +112,37 @@ int main(int argc, char *argv[]) {
         u.version,
         u.machine);
 
-    // allocate: https://man7.org/linux/man-pages/man3/malloc.3.html
-    // we allocate a way larger area than needed (2MB / thread) to avoid contention at the kernel's PTE lock
-    unsigned long* my_mmap_ptr = (unsigned long*)mmap(NULL, threads*HUGEPAGE_SIZE, PROT_NONE, MAP_ANON|MAP_SHARED, -1, 0);
-    if (my_mmap_ptr == NULL) {
-        printf("mmap() failed and returned NULL\n");
-        return -1;
-    } else if (my_mmap_ptr == MAP_FAILED) {
-        printf("mmap() failed with MAP_FAILED: %s\n", strerror(errno));
-        return -1;
-    } else {
-        printf("allocated %ld bytes (%ldMB) at %p\n", threads*HUGEPAGE_SIZE, threads*HUGEPAGE_SIZE/(4096*256), my_mmap_ptr);
-    }
-
     for (long i=0; i<threads; i++) {
         thread_infos[i].tid = i; // assign each thread an id
-        thread_infos[i].my_page = my_mmap_ptr + i * HUGEPAGE_SIZE / sizeof(unsigned long); // assign each thread a page
-        printf("fault-in thread %ld's page\n", i);
-        thread_infos[i].return_value = mprotect(thread_infos[i].my_page, PAGE_SIZE, PROT_WRITE); // make thread's page writable
-        thread_infos[i].my_page[0] = 0; // fault-in thread's page
 
-        // // set cpu affinities: https://man7.org/linux/man-pages/man3/pthread_setaffinity_np.3.html
-        // CPU_ZERO(&thread_infos[i].cpuset);
-        // CPU_SET(i, &thread_infos[i].cpuset);
-        // if (i == 0) {
-        //     // set main thread's cpu affinity, which is already running
-        //     pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &thread_infos[i].cpuset);
-        // } else {
-        //     // set child threads' cpu affinities
-        //     pthread_attr_init(&thread_infos[i].attr);
-        //     pthread_attr_setaffinity_np(&thread_infos[i].attr, sizeof(cpu_set_t), &thread_infos[i].cpuset); // reusing these below is fine
-        // }
-        // printf("cpu affinities set\n");
+        // allocate: https://man7.org/linux/man-pages/man3/malloc.3.html
+        // we allocate a way larger area than needed (2MB / thread) to avoid contention at the kernel's PTE locks
+        thread_infos[i].my_page = (unsigned long*)mmap(NULL, HUGEPAGE_SIZE, PROT_NONE, MAP_ANON|MAP_SHARED, -1, 0); // assign each thread a page
+        if (thread_infos[i].my_page == NULL) {
+            printf("mmap() failed and returned NULL\n");
+        return -1;
+        } else if (thread_infos[i].my_page == MAP_FAILED) {
+            printf("mmap() failed with MAP_FAILED: %s\n", strerror(errno));
+            return -1;
+        } else {
+            printf("allocated %d bytes (%dMB) at %p\n", HUGEPAGE_SIZE, HUGEPAGE_SIZE/(4096*256), thread_infos[i].my_page);
+        }
+        thread_infos[i].return_value = mprotect(thread_infos[i].my_page, PAGE_SIZE, PROT_READ|PROT_WRITE); // make thread's page writable
+        // TODO check mprotect return value
+        printf("fault-in thread %ld's page\n", i);
+        thread_infos[i].my_page[0] = 0; // fault-in thread's page
+        // set cpu affinities: https://man7.org/linux/man-pages/man3/pthread_setaffinity_np.3.html
+        CPU_ZERO(&thread_infos[i].cpuset);
+        CPU_SET(i, &thread_infos[i].cpuset);
+        if (i == 0) {
+        // set main thread's cpu affinity, which is already running
+            pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &thread_infos[i].cpuset);
+        } else {
+            // set child threads' cpu affinities
+            pthread_attr_init(&thread_infos[i].attr);
+            pthread_attr_setaffinity_np(&thread_infos[i].attr, sizeof(cpu_set_t), &thread_infos[i].cpuset); // reusing these below is fine
+        }
+        printf("cpu affinities set\n");
     }
     
     printf("\nbegin benchmarking\n\n");
@@ -147,8 +150,15 @@ int main(int argc, char *argv[]) {
     // main microbenchmarking loops
     for (long t=0; t<threads; t++) {
         for (int smokewagon=0; smokewagon<2; smokewagon++) {
-            // smokewagon 0 means don't use it, 1 means use it
-            madvise(my_mmap_ptr, threads*HUGEPAGE_SIZE, smokewagon ? 26 : 27); // 26 is MADV_PRIVATE_TLB, 27 is MADV_NORMAL_TLB
+        for (int readwrite=0; readwrite<2; readwrite++) {
+            // smokewagon 0 means don't use it, 1 means clear it
+            for (long i=0; i<=t; i++) {
+                madvise(thread_infos[i].my_page, PAGE_SIZE, smokewagon ? 26 : 27); // 26 is MADV_PRIVATE_TLB, 27 is MADV_NORMAL_TLB
+            }
+
+            // readwrite 0 means we're alternating, readwrite 1 means both mprotects are read and write and so won't shootdown
+            protread  = readwrite ? PROT_READ|PROT_WRITE : PROT_READ;
+            protwrite = readwrite ? PROT_READ|PROT_WRITE : PROT_WRITE;
 
             // set when benchmark ends
             struct timespec now;
@@ -156,7 +166,7 @@ int main(int argc, char *argv[]) {
             end = (duration + now.tv_sec) * 1000000000L + now.tv_nsec;
             assert(end > now.tv_sec * 1000000000L + now.tv_nsec); // check overflow
 
-            printf("Running %s mprotect loop with %ld threads for %ld seconds:\n", smokewagon ? "smokewagon" : "baseline", t+1, duration);
+            printf("Running %s-%s mprotect loop with %ld threads for %ld seconds:\n", smokewagon ? "smokewagon" : "baseline", readwrite ? "no_prot_change" : "shootdown", t+1, duration);
 
             // create and run threads
             for (long i=1; i<=t; i++) {
@@ -175,10 +185,11 @@ int main(int argc, char *argv[]) {
 
             // sum counters from each thread
             for (long i=0; i<=t; i++) {
-                results[t][smokewagon] += thread_infos[i].counter;
+                results[t][smokewagon][readwrite] += thread_infos[i].counter;
                 printf("tid %ld performed %lu loops\n", i, thread_infos[i].counter);
             }
-            printf("%ld threads performed %ld %s loops in %ld seconds.\n\n", t+1, results[t][smokewagon], smokewagon ? "smokewagon" : "baseline", duration);
+            printf("%ld threads performed %ld %s-%s loops in %ld seconds.\n\n", t+1, results[t][smokewagon][readwrite], smokewagon ? "smokewagon" : "baseline", readwrite ? "no_prot_change" : "shootdown", duration);
+        }
         }
     }
 
@@ -199,22 +210,25 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    fprintf(fptr, "threads, baseline, smokewagon\n");
+    fprintf(fptr, "threads, baseline-shootdown, baseline-none, smokewagon-shootdown, smokewagon-none\n");
     for (long t=0; t<threads; t++) {
-        fprintf(fptr, "%ld, %ld, %ld\n", t+1, results[t][0], results[t][1]);
+        fprintf(fptr, "%ld, %ld, %ld, %ld, %ld\n", t+1, results[t][0][0], results[t][0][1], results[t][1][0], results[t][1][1]);
     }
     fclose(fptr);
     printf("totals written to %s\n", filename);
 
     free(filename);
 
-    // for (long t=1; t<threads; t++) {
-    //     pthread_attr_destroy(&thread_infos[t].attr);
-    // /* don't destroy pthread_attr 0, since we didn't initialize it */
-    // }
-    // printf("pthread attributes destroyed\n");
+    /* don't destroy pthread_attr for t=0, since we didn't initialize it */
+    for (long t=1; t<threads; t++) {
+        pthread_attr_destroy(&thread_infos[t].attr);
+    }
+    printf("pthread attributes destroyed\n");
 
-    munmap(my_mmap_ptr, threads*HUGEPAGE_SIZE);
+    for (long i=0; i<threads; i++) {
+        munmap(thread_infos[i].my_page, HUGEPAGE_SIZE);
+    }
+    
     printf("testing area munmapped\n");
 
     return EXIT_SUCCESS;
