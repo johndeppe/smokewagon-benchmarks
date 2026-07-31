2b0dad2ba4797a79095e4173f84a39a99a377a1a
John Henry Deppe <deppe@ece.ubc.ca>
2026-07-30 23:00:30 -0700

    add exercise-fork-madvise.c
    A first hack at testing fork() support.

---
+++ exercise-fork-madvise.c
@@ -0,0 +1,93 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define PAGE_SIZE   4096
+#define PAGES       8         // even number
+#define NUM_THREADS    64
+
+void* test_smokewagon(void* tid) {
+    char* ptr;
+    int result;
+    bool valid = true;
+
+    // allocate: https://man7.org/linux/man-pages/man3/malloc.3.html
+    result = posix_memalign( (void**)&ptr, PAGE_SIZE, PAGES*PAGE_SIZE );
+    if (result != 0) {
+        printf("tid %ld: posix_memaligned() failed and returned: %d\n", (long) tid, result);
+        return (void*) -1;
+    }
+
+    // touch first half:
+    for (long i = 0; i < PAGES*PAGE_SIZE/2; i++ ) {
+        ptr[i] = 'x';
+    }
+    printf("tid %ld: touched first half of allocated memory.\n", (long) tid);
+
+    // madvise: https://man7.org/linux/man-pages/man2/madvise.2.html
+    result = madvise(ptr, PAGES*PAGE_SIZE, 26); // 26 advises MADV_PRIVATE_TLB
+    if (result != 0) {
+        printf("tid %ld: madvise smokewagonified: %d, errno: %d, strerror: %s\n", (long) tid, result, errno, strerror(errno));
+    }
+
+    // fork
+    pid_t my_pid = fork();
+    printf("tid %ld: pid: %lu: forked.\n", (long) tid, (unsigned long) my_pid);
+
+    // touch second half:
+    for (long i = PAGES*PAGE_SIZE/2; i < PAGES*PAGE_SIZE; i++ ) {
+        ptr[i] = 'x';
+    }
+    printf("tid %ld: pid: %lu touched all allocated memory.\n", (long) tid, (unsigned long) my_pid);
+
+    // check correctness
+    for (long i = 0; i < PAGES*PAGE_SIZE; i++) {
+	if (ptr[i] != 'x') {
+	    printf("tid %ld: oops, ptr[%ld] is '%c' instead of 'x'\n", (long) tid, i, ptr[i]);
+	    valid = false;
+	    }
+    }
+
+    // normalize
+    result = madvise(ptr, PAGES*PAGE_SIZE, 27); // 27 advises MADV_NORMAL_TLB
+    if (result != 0) {
+	    printf("tid %ld: pid: %lu: madvise normalized: %d, errno: %d, strerror: %s\n", (long) tid, (unsigned long) my_pid, result, errno, strerror(errno));
+    }
+
+    // free
+    free(ptr);
+    if (valid == true) {
+        return (void*) 0;
+    } else {
+        return (void*) -1;
+    }
+}
+
+int main(void) {
+    pthread_t threads[NUM_THREADS];
+    int returns[NUM_THREADS];
+    long ret;
+
+
+    // create threads
+    for (long i=0;i<NUM_THREADS;i++) {
+        int ret = pthread_create(&threads[i], NULL, test_smokewagon, (void*) i);
+        if (ret) printf("ERROR: return code for thread %ld from pthread_create() is %d\n", i, ret);
+    }
+
+    // join created threads
+    for (long i=0;i<NUM_THREADS;i++) {
+        int ret = pthread_join(threads[i], NULL);
+        if (ret) printf("ERROR: return code %d from thread %ld\n", ret, i);
+    }
+}
+
+/* Things to try:
+ * 1. Not clearing the entire madvise.
+ */
+
