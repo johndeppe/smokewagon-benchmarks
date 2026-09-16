b2038ced1cb1562551c08251ee28b66604a2ff96
John Henry Deppe <deppe@ece.ubc.ca>
2026-09-16 09:34:02 -0700

    wip: mmap and apache after xarray fix
    After fixing the kernel free Smokewagon entries after munmap().
    This commit is our results after fixing the kernel to free Smokewagon
    entries after munmap().
    
    We also add a flag for controlling wether or not the mmap()
    microbenchmark adds a bystander page to the one being cycled.

--- apache/benchmark-baseline-script.sh
+++ apache/benchmark-baseline-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-for i in {1..20}
+for i in {1..56}
 do
     echo starting apache with $i core\(s\)
     sudo taskset -c 1-$i /opt/smokewagon/httpd/bin/httpd -k start -f /home/milkv/sophgo/smokewagon-benchmarks/apache/httpd.conf
 
     echo hey
-    taskset -c 57-63 /home/milkv/go/bin/hey -disable-keepalive -z 10s http://localhost/ > /home/milkv/sophgo/smokewagon-benchmarks/apache/results/hey-output-inactive-$(printf '%02d' "$i").txt
+    taskset -c 57-63 /home/milkv/go/bin/hey -disable-keepalive -z 10s http://localhost/index.html > /home/milkv/sophgo/smokewagon-benchmarks/apache/results/hey-output-baseline-$(printf '%02d' "$i").txt
 
     echo shutting down apache
     sudo /opt/smokewagon/httpd/bin/httpd -k graceful-stop -f /home/milkv/sophgo/smokewagon-benchmarks/apache/httpd.conf
